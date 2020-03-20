e69f12d9dabebbacf18d15d87890719698ea0bad
olaniyi <ajayiolaniyi8@gmail.com>
2020-03-20 06:48:27 +0100

    template inheritance

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -6,3 +6,9 @@ def index(request):
     title = "This is index page"
     context = {"title":title}
     return render(request, "index.html", context)
+
+
+def about(request):
+    title = "This is about us page"
+    context = {"title":title}
+    return render(request, "about.html", context)
