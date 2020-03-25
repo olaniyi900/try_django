9eb598ac1c180f3bee1d120cf63c1755a1a603b7
olaniyi <ajayiolaniyi8@gmail.com>
2020-03-25 16:51:15 +0100

    uploading pic for profile

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,4 +9,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
