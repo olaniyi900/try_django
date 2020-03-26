3eb052b8df86ca1bbe2f9bb5911f425d82e353f1
olaniyi <ajayiolaniyi8@gmail.com>
2020-03-26 14:09:20 +0100

    login require for product

--- blog/product/views.py
+++ blog/product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Product
 from .forms import ProductCreateForm
 
@@ -19,7 +20,7 @@ def product_detail(request, p_id):
     }
     return render(request, template_name, context)
 
-
+@login_required
 def product_create(request):
     if request.method == 'POST':
         form = ProductCreateForm(request.POST)
@@ -35,13 +36,15 @@ def product_create(request):
         
     return render(request, template_name, context)
 
+@login_required
 def product_update(request, p_id):
     product = Product.objects.get(pk=p_id)
     if request.method == 'POST':
        
         form = ProductCreateForm(request.POST, instance=product)
         if form.is_valid():
-            Product.objects.create(**form.cleaned_data)
+            Product(**form.cleaned_data)
+            product.save()
             return redirect('product_home')
 
     
